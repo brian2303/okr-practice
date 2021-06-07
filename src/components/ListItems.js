39b22121e5dc98277fb11eb4d83f0e736346d2fd
Brian olarte <brayan_andrey23@hotmail.com>
2021-06-06 20:26:44 -0500

    plantilla base

---
+++ src/components/ListItems.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PeopleIcon from '@material-ui/icons/People';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LayersIcon from '@material-ui/icons/Layers';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+
+export const mainListItems = (
+  <div>
+    <ListItem button>
+      <Link to="/okr/list" style={{ textDecoration: 'none' }} >
+        <Button variant="contained" style={{ background: 'orange', color: 'white' }}>
+          <AssignmentIcon />
+          Mis OKR
+        </Button>
+      </Link>
+    </ListItem>
+  </div>
+);
+
+export const secondaryListItems = (
+  <div>
+    <ListSubheader inset>Saved reports</ListSubheader>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItem>
+  </div>
+);
---
+++ src/pages/OkrList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OkrList = () => {
+  return (
+    <div>
+      OKR LIST
+    </div>
+  );
+}
+
+export default OkrList;
